91ebd0e44271853b609fd28bbd10d38292601b87
khj <gw2159@dndmobile.com>
2025-11-20 17:07:43 +0900

    Appearance 가이드 작업중

---
+++ src/app/scBank-components/component/Appearance.tsx
@@ -0,0 +1,127 @@
+"use client";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/index";
+import "../../globals.css";
+
+import * as React from "react";
+
+const appearanceList = [
+  {
+    name: "spacing",
+    value: [
+      { value: 0 },
+      { value: 2 },
+      { value: 4 },
+      { value: 8 },
+      { value: 12 },
+      { value: 16 },
+      { value: 20 },
+      { value: 24 },
+      { value: 32 },
+      { value: 40 },
+      { value: 48 },
+      { value: 64 },
+    ],
+  },
+  {
+    name: "radius",
+    value: [
+      { value: 4 },
+      { value: 8 },
+      { value: 12 },
+      { value: 16 },
+      { value: "rounded" },
+    ],
+  },
+];
+
+const AppearanceSample = React.forwardRef<HTMLDivElement>(() => {
+  const titleStyle = "text-[20px] text-neutral-800 font-bold";
+
+  return (
+    <>
+      {appearanceList.map((item) => (
+        <div key={item.name} className="flex flex-col gap-2 mt-6">
+          <p className={titleStyle}>{item.name}</p>
+          <Table className="bg-white">
+            {/* unit */}
+            <TableHeader className="bg-neutral-800 ">
+              <TableRow>
+                <TableHead className="text-neutral-50 text-center not-[]:w-[200px]">
+                  Variable Name
+                </TableHead>
+                <TableHead className="text-neutral-50 text-center">
+                  Primitive Variable
+                </TableHead>
+                <TableHead className="text-neutral-50 text-center">
+                  Value(px)
+                </TableHead>
+                <TableHead className="text-neutral-50 text-center">
+                  {item.name === "spacing" && `Value(REM)`}
+                  {item.name === "radius" && `VISUAL REPRESENTATION`}
+                </TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {item.value.map((unit) => {
+                const isSpacing = item.name === "spacing";
+                const isRadius = item.name === "radius";
+                const val = unit.value;
+
+                return (
+                  <TableRow key={`${item.name}-${val}`}>
+                    {/* Variable Name */}
+                    <TableCell className="py-4 text-center not-even:font-medium">
+                      {isSpacing && `spacing/${val}`}
+                      {isRadius && `border/radius/${val}`}
+                    </TableCell>
+
+                    {/* Primitive Variable */}
+                    <TableCell className="py-4 text-center">
+                      {isSpacing && `unit/${val}`}
+                      {isRadius &&
+                        (typeof val === "number" ? `${val} px` : "-")}
+                    </TableCell>
+
+                    {/* Value(px) */}
+                    <TableCell className="py-4 text-center">
+                      {typeof val === "number" ? `${val} px` : "-"}
+                    </TableCell>
+
+                    {/* Preview Sample */}
+                    <TableCell className="py-4 text-center">
+                      {isSpacing && (
+                        <div
+                          className="h-[20] sc-bg-primary"
+                          style={{ width: `${val}px` }}
+                        />
+                      )}
+
+                      {isRadius && (
+                        <div
+                          className="w-[40] h-[40] sc-bg-primary"
+                          style={{
+                            borderRadius:
+                              typeof val === "number" ? `${val}px` : "100%",
+                          }}
+                        />
+                      )}
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </div>
+      ))}
+    </>
+  );
+});
+
+export default AppearanceSample;
--- src/app/scBank-components/page.tsx
+++ src/app/scBank-components/page.tsx
@@ -1,18 +1,19 @@
 import React from "react";
-import { Tabs, TabsList, TabsTrigger, TabsContent, } from "@/components/index";
+import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/index";
 import { ScButton } from "@/app/scBank-components/component/ui/index";
-import ColorSample from './component/color';
-import TypographySample from './component/Typography';
-
+import ColorSample from "./component/color";
+import TypographySample from "./component/Typography";
+import AppearanceSample from "./component/Appearance";
 
 // SC은행 컴포넌트 가이드
 export default function ComponentGuidePage() {
- 
   return (
     <div className="min-h-screen py-12 px-6 bg-gray-200 dark:bg-gray-900">
       <div className="max-w-5xl mx-auto">
         <header className="mb-8">
-          <h1 className="text-3xl font-extrabold text-gray-900 dark:text-gray-100">SC은행 컴포넌트 가이드</h1>
+          <h1 className="text-3xl font-extrabold text-gray-900 dark:text-gray-100">
+            SC은행 컴포넌트 가이드
+          </h1>
           {/* <p className="mt-2 text-gray-600 dark:text-gray-300">
             Shadcn UI 설치된 컴포넌트 예제와 사용방법을 가이드페이지입니다.
           </p>
@@ -28,18 +29,27 @@ export default function ComponentGuidePage() {
               <TabsTrigger value="Color">Color</TabsTrigger>
               <TabsTrigger value="Typography">Typography</TabsTrigger>
               <TabsTrigger value="scComponent">scComponent</TabsTrigger>
+              <TabsTrigger value="Appearance">Appearance</TabsTrigger>
             </TabsList>
-            <TabsContent value="Color" className="flex flex-col gap-[24] pt-[12]">
+            <TabsContent
+              value="Color"
+              className="flex flex-col gap-[24] pt-[12]"
+            >
               <ColorSample />
             </TabsContent>
-            <TabsContent value="Typography"><TypographySample/></TabsContent>
+            <TabsContent value="Typography">
+              <TypographySample />
+            </TabsContent>
             <TabsContent value="scComponent">
               <div>
-                <ScButton type="button" >버튼</ScButton>
-                <ScButton type="button" disabled={true}>버튼</ScButton>
+                <ScButton type="button">버튼</ScButton>
+                <ScButton type="button" disabled={true}>
+                  버튼
+                </ScButton>
               </div>
-
-
+            </TabsContent>
+            <TabsContent value="Appearance">
+              <AppearanceSample />
             </TabsContent>
           </Tabs>
         </div>
