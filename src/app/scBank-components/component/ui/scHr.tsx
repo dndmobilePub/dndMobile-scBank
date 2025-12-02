c15d321e8af6d83736d5f85b930f2cff56c0135a
bluewebd <bluewebd@dndmobile.co.kr>
2025-12-02 11:25:01 +0900

    백 Hr태그 수정

--- src/app/scBank-components/component/ui/scHr.tsx
+++ src/app/scBank-components/component/ui/scHr.tsx
@@ -14,9 +14,10 @@ const hrStyle = [
 export const ScHr =({type, size, className, ...rest}: HrProps) =>{
     const typeName = hrStyle.find(style => style.typeName === type)
     const tyleClass = typeName? typeName.className : 'line'
+    const borderSize = `border-t-${size}`
 
     return(
-        <hr className={cn(`${tyleClass} border-<${size? size : 1}> sc-neutral-200` , className)} {...rest}/>
+        <hr className={cn(tyleClass, size? borderSize : 'border-t', 'sc-neutral-200' , className)} {...rest}/>
     );
 
 };
